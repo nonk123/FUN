2e98f42691e8cf2c149c2a096b165c4c2ca1d637
nonk123 <me@nonk.dev>
2025-11-28 15:01:00 +0300

    `__log` docs clarification

--- include/log.h
+++ include/log.h
@@ -4,6 +4,9 @@
 
 void log_init();
 
+/// Log with filename and line number for easier tracing.
+///
+/// Please use the macros from this header instead of calling this function directly.
 void __log(TraceLogLevel level, int linum, const char* file, const char* fmt, ...);
 #define _log(level, ...) __log(level, __LINE__, __FILE__, __VA_ARGS__)
 
